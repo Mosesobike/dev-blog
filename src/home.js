14b7b29d65626f5c37555f8a4332e09e1d2a71ac
moses <mosesobike@gmail.com>
2021-02-24 22:05:19 +0100

    added the blog list

--- src/home.js
+++ src/home.js
@@ -1,12 +1,16 @@
-const Home = () => {
+import {useState} from "react";
+import Bloglist from './Bloglist';
 
-const handleClick= () =>{
-    alert("what is your name");
-};
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+      {title: 'My new website', body:'lorem ipusm....',author:'mario', id: 1},
+      {title: 'Welcome party', body:'lorem ipusm....',author:'moses', id: 2},
+      {title: 'Web dev top tips', body:'lorem ipusm....',author:'obike', id: 3}
+  ]);
+
     return ( 
         <div className ="home">
-        <h1>Home Page</h1>
-        <button onClick ={handleClick}>Click Me</button>
+          <Bloglist blogs={blogs}  title = "All blogs"/>
         </div>
      );
 };
